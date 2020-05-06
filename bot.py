8458e6998e66b80bc0538eb705339167cf7a1452
Dominik Haitz <dominik.haitz@gmx.de>
2020-05-06 22:53:18 +0200

    fix

--- bot.py
+++ bot.py
@@ -72,15 +72,10 @@ def main(wait_time_hours):
             if len(text) <= max_tweet_size:
                 chunks = [text]
 
-                ##
-                continue
-                ##
-
-
             else:
                 chunks = get_splitted_texts(text, max_tweet_size)
                 chunks = list(chunks)
-                chunks = [chunk.replace('/0', '/'+str(len(chunks))) for chunk in chunks]
+                chunks = [chunk.replace('/0', f'/{len(chunks)}' for chunk in chunks]
 
             tweet_id = None
             for chunk in chunks:
