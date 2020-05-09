25521011651640d67032da1b3a7c6b0fca837839
Dominik Haitz <dominik.haitz@gmx.de>
2020-05-09 21:58:06 +0200

    refactor

--- bot.py
+++ bot.py
@@ -8,6 +8,13 @@ import datetime
 
 MAX_TWEET_SIZE = 280
 
+WAIT_TIME_HOURS_DEFAULT = 24
+WAIT_TIME_HOURS = float(os.getenv("WAIT_TIME_HOURS", default=WAIT_TIME_HOURS_DEFAULT))
+
+START_HOUR_DEFAULT = 10
+START_HOUR = int(os.getenv('START_HOUR', default=START_HOUR_DEFAULT))
+
+
 def create_api():
     consumer_key = os.getenv("CONSUMER_KEY")
     consumer_secret = os.getenv("CONSUMER_SECRET")
@@ -28,20 +35,21 @@ def create_api():
     logging.info("API created")
     return api
 
-def get_texts(shuffle=False):
-    with open('tweet_short.txt', 'r') as f:
-        lines = f.read().splitlines()
+
+def get_texts(input_file: str, shuffle=False) -> list:
+
+    with open(input_file, 'r') as f:
+        texts = f.read().splitlines()
 
     if shuffle:
-        random.shuffle(lines)
+        random.shuffle(texts)
 
-    return lines
+    return texts
 
 
+def get_individual_tweets_from_text(text, max_length=MAX_TWEET_SIZE) -> list:
 
-def get_splitted_texts(s):
-
-    words = s.split()
+    words = text.split()
 
     # start string with first word, then begin loop on second
     tweet_number = 1
@@ -49,29 +57,29 @@ def get_splitted_texts(s):
 
     for word in words:
 
-        if len(new_tweet + ' ' + word + ' ...') < MAX_TWEET_SIZE:  # if tweet is short enough with word appended, keep appending
+        if len(new_tweet + ' ' + word + ' ...') < max_length:  # if tweet is short enough with word appended, keep appending
             new_tweet = new_tweet + ' ' + word
         else:
             yield new_tweet + ' ...'
             tweet_number += 1
             new_tweet = f"{tweet_number}/0 ... " + word
-    yield  new_tweet
+    yield new_tweet
 
 
-
-def wait_until_certain_hour_to_start(start_hour):
+def wait_until_certain_hour_to_start(start_hour: int) -> None:
     while datetime.datetime.now().hour != start_hour:
         logging.info("Current hour: {datetime.datetime.now().hour},  start hour: {start_hour}  -> waiting ...")
         time.sleep(30*60)   # wait half an hour, then check again
     logging.info("Current hour: {datetime.datetime.now().hour} ==  start hour: {start_hour}")
 
-def main(wait_time_hours):
+
+def main(wait_time_hours: int, input_file: str) -> None:
     api = create_api()
 
-    tweet_texts = get_texts(shuffle=True)
+    texts = get_texts(input_file, shuffle=True)
 
     while True:
-        for text in tweet_texts:
+        for text in texts:
 
             try:
 
@@ -79,24 +87,23 @@ def main(wait_time_hours):
 
                 # single tweets can not be longer, so split into chunks
                 if len(text) <= MAX_TWEET_SIZE:
-                    chunks = [text]
+                    tweets = [text]
 
                 else:
-                    chunks = get_splitted_texts(text)
-                    chunks = list(chunks)
-                    chunks = [chunk.replace('/0', f'/{len(chunks)}') for chunk in chunks]
+                    tweets = list(get_individual_tweets_from_text(text))
+                    tweets = [tweet.replace('/0', f'/{len(tweets)}') for tweet in tweets]
 
                 tweet_id = None
-                for chunk in chunks:
+                for tweet in tweets:
 
-                    logging.info(chunk)
-                    call_return_status = api.update_status(chunk, tweet_id)
+                    logging.info(tweet)
+
+                    call_return_status = api.update_status(tweet, tweet_id)
+                    logging.info(call_return_status)
 
                     tweet_id = call_return_status.id
 
-                    logging.info(call_return_status)
-
-                time.sleep(60*60*wait_time_hours)  # wait 24h
+                time.sleep(60*60*wait_time_hours)  # turn hours into seconds
 
             except tweepy.error.TweepError as e:
                 logging.error(e)
@@ -104,6 +111,6 @@ def main(wait_time_hours):
 
 if __name__ == '__main__':
 
-    wait_until_certain_hour_to_start(start_hour=int(os.getenv('START_HOUR')))
+    wait_until_certain_hour_to_start(start_hour=START_HOUR)
 
-    main(wait_time_hours=float(os.getenv("WAIT_TIME_HOURS")))
+    main(wait_time_hours=WAIT_TIME_HOURS, input_file='tweet_short.txt')
