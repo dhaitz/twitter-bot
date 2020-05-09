9fccc109b50b49d9eacc62f18dcf1f2c18f516d5
Dominik Haitz <dominik.haitz@gmx.de>
2020-05-09 22:19:50 +0200

    add unit tests

---
+++ tests.py
@@ -0,0 +1,18 @@
+import bot
+
+
+def test_split_long_string():
+    text = 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    tweets = bot.get_individual_tweets_from_text(text)
+
+    correct_answer = [
+        "1/2 Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate ...",
+        "2/2 ... velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ]
+    assert tweets == correct_answer
+
+
+def test_dont_split_short_string():
+    text = 'Lorem ipsum'
+    tweets = bot.get_individual_tweets_from_text(text)
+    assert tweets == [text]
