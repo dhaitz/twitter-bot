391c2eb7891c472aca0ad236f8f8780713ad93b4
Dominik Haitz <dominik.haitz@gmx.de>
2020-05-09 22:19:44 +0200

    further refactoring

--- bot.py
+++ bot.py
@@ -15,7 +15,38 @@ START_HOUR_DEFAULT = 10
 START_HOUR = int(os.getenv('START_HOUR', default=START_HOUR_DEFAULT))
 
 
-def create_api():
+def run_bot(input_file: str, wait_time_hours: float) -> None:
+    """Get texts from file, format into tweets, tweet and wait"""
+    api = create_api()
+
+    texts = get_texts(input_file, shuffle=True)
+
+    while True:
+        for text in texts:
+            logging.info(text)
+            tweets = get_individual_tweets_from_text(text)
+
+            try:
+
+                # For multiple tweets in a thread, we have to reply to the previous tweet via its id.
+                # The first tweet replies to tweet id None -> not a reply
+                id_of_thread_tweet_to_reply_to = None
+                for tweet in tweets:
+
+                    logging.info(tweet)
+
+                    call_return_status = api.update_status(tweet, id_of_thread_tweet_to_reply_to)
+                    logging.info(call_return_status)
+
+                    id_of_thread_tweet_to_reply_to = call_return_status.id
+
+                time.sleep(60*60*wait_time_hours)  # turn hours into seconds
+
+            except tweepy.error.TweepError as e:
+                logging.error(e)
+
+
+def create_api() -> tweepy.API:
     consumer_key = os.getenv("CONSUMER_KEY")
     consumer_secret = os.getenv("CONSUMER_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
@@ -48,6 +79,17 @@ def get_texts(input_file: str, shuffle=False) -> list:
 
 
 def get_individual_tweets_from_text(text, max_length=MAX_TWEET_SIZE) -> list:
+    """Check whether we can take the text as-is as tweet or have to split it into multiple tweets"""
+    if len(text) <= max_length:
+        tweets = [text]
+    else:
+        tweets = list(split_text_into_multiple_tweets(text, max_length))
+        tweets = [tweet.replace('/0', f'/{len(tweets)}') for tweet in tweets]
+
+    return tweets
+
+
+def split_text_into_multiple_tweets(text: str, max_length: int = MAX_TWEET_SIZE) -> list:
 
     words = text.split()
 
@@ -67,50 +109,15 @@ def get_individual_tweets_from_text(text, max_length=MAX_TWEET_SIZE) -> list:
 
 
 def wait_until_certain_hour_to_start(start_hour: int) -> None:
+    """Tweeting should start at a certain hour, not when the script starts"""
     while datetime.datetime.now().hour != start_hour:
         logging.info("Current hour: {datetime.datetime.now().hour},  start hour: {start_hour}  -> waiting ...")
         time.sleep(30*60)   # wait half an hour, then check again
     logging.info("Current hour: {datetime.datetime.now().hour} ==  start hour: {start_hour}")
 
 
-def main(wait_time_hours: int, input_file: str) -> None:
-    api = create_api()
-
-    texts = get_texts(input_file, shuffle=True)
-
-    while True:
-        for text in texts:
-
-            try:
-
-                logging.info(text)
-
-                # single tweets can not be longer, so split into chunks
-                if len(text) <= MAX_TWEET_SIZE:
-                    tweets = [text]
-
-                else:
-                    tweets = list(get_individual_tweets_from_text(text))
-                    tweets = [tweet.replace('/0', f'/{len(tweets)}') for tweet in tweets]
-
-                tweet_id = None
-                for tweet in tweets:
-
-                    logging.info(tweet)
-
-                    call_return_status = api.update_status(tweet, tweet_id)
-                    logging.info(call_return_status)
-
-                    tweet_id = call_return_status.id
-
-                time.sleep(60*60*wait_time_hours)  # turn hours into seconds
-
-            except tweepy.error.TweepError as e:
-                logging.error(e)
-
-
 if __name__ == '__main__':
 
     wait_until_certain_hour_to_start(start_hour=START_HOUR)
 
-    main(wait_time_hours=WAIT_TIME_HOURS, input_file='tweet_short.txt')
+    run_bot(wait_time_hours=WAIT_TIME_HOURS, input_file='tweet_short.txt')
